ab5b9818408a7314d0ae0573df69adcf6425508a
Actat <akiotoshima@gmail.com>
2019-10-13 21:28:22 +0900

    remove lookAt (when OrbitControls works, lookAt doesn't work)

--- 3d2d.js
+++ 3d2d.js
@@ -7,7 +7,6 @@ renderer.domElement.id = "canvas";
 document.body.appendChild(renderer.domElement);
 
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.lookAt(0, 0, 0);
 
 var controls = new THREE.OrbitControls(camera, renderer.domElement);
 resetCameraPosition();
