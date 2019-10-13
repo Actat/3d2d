f0af19a7608a7f6493cc48d004f675a952d5cb4a
Actat <akiotoshima@gmail.com>
2019-10-13 15:12:36 +0900

    transparent background

--- 3d2d.js
+++ 3d2d.js
@@ -1,19 +1,29 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
 var renderer = new THREE.WebGLRenderer({
-    preserveDrawingBuffer: true
+    preserveDrawingBuffer: true,
+    alpha: true
 });
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(window.innerWidth / 2.0, window.innerHeight / 2.0);
 renderer.domElement.id = "canvas";
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 0, 100);
+camera.lookAt(0, 0, 0);
 
-camera.position.z = 5;
+var scene = new THREE.Scene();
+
+var material = new THREE.LineBasicMaterial({color: 0x000000});
+
+var geometry = new THREE.Geometry();
+geometry.vertices.push(new THREE.Vector3(-10, 0, 0));
+geometry.vertices.push(new THREE.Vector3(0, 10, 0));
+geometry.vertices.push(new THREE.Vector3(10, 0, 0));
+var line = new THREE.Line(geometry, material);
+scene.add(line);
+
+var geometry = new THREE.CircleGeometry(10, 64, Math.PI, Math.PI);
+geometry.vertices.shift();
+scene.add(new THREE.Line(geometry, material));
 
 function animate() {
     requestAnimationFrame(animate);
